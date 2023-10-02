e2cc4c2b38153cca8c4447c69c01cd5bb9355bde
Jeremy Whiting <jeremy.whiting@collabora.com>
2023-10-02 12:31:28 -0600

    Add basic running script functionality.
    Will likely move api methods out to be more generic, but this
    runs for now and gives true on success, false on failure, etc.
    May also need to change from process.poll to wait for longer
    running scripts. will do once tested more.

--- src/manager.rs
+++ src/manager.rs
@@ -23,19 +23,39 @@
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
+use subprocess::{ExitStatus::Exited, Popen, PopenConfig, Redirection};
 use zbus_macros::dbus_interface;
 use zbus::{ObjectServer, SignalContext, MessageHeader};
 pub struct SMManager {
+}
+
+fn run_script(script: String) -> bool {
+    // Run given script and return true on success
+    let mut process = Popen::create(&[script], PopenConfig {
+        stdout: Redirection::Pipe, ..Default::default()
+    }).unwrap();
+    let (out, err) = process.communicate(None).unwrap();
+    if let Some(exit_status) = process.poll() {
+        return exit_status == Exited(0);
+    } else {
+        return false;
+    }
 }
 
 #[dbus_interface(name = "com.steampowered.SteamOSManager1")]
 impl SMManager {
     const API_VERSION: u32 = 1;
 
+
     async fn say_hello(&self, name: &str) -> String {
         format!("Hello {}!", name)
     }
     
+    async fn factory_reset(&self) -> bool {
+        // Run steamos factory reset script and return true on success
+        return run_script("steamos-factory-reset".to_string());
+    }
+
     /// A version property.
     #[dbus_interface(property)]
     async fn version(&self) -> u32 {
