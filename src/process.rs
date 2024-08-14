bcab18c5fb9a62fd30d8b4a2a57eb9668c92328a
Vicki Pfau <vi@endrift.com>
2024-08-13 21:34:41 -0700

    process: Clean up P: AsRef<> into impl AsRef<> where applicable

--- src/process.rs
+++ src/process.rs
@@ -13,8 +13,8 @@ use std::process::Stdio;
 use tokio::process::Command;
 
 #[cfg(not(test))]
-pub async fn script_exit_code<P: AsRef<OsStr>>(
-    executable: P,
+pub async fn script_exit_code(
+    executable: impl AsRef<OsStr>,
     args: &[impl AsRef<OsStr>],
 ) -> Result<i32> {
     // Run given script and return the exit code
@@ -28,17 +28,14 @@ pub async fn script_exit_code<P: AsRef<OsStr>>(
 }
 
 #[cfg(test)]
-pub async fn script_exit_code<P: AsRef<OsStr>>(
-    executable: P,
-    args: &[impl AsRef<OsStr>],
-) -> Result<i32> {
+pub async fn script_exit_code(executable: AsRef<OsStr>, args: &[impl AsRef<OsStr>]) -> Result<i32> {
     let test = crate::testing::current();
     let args: Vec<&OsStr> = args.iter().map(|arg| arg.as_ref()).collect();
     let cb = test.process_cb.get();
     cb(executable.as_ref(), args.as_ref()).map(|(res, _)| res)
 }
 
-pub async fn run_script<P: AsRef<OsStr>>(executable: P, args: &[impl AsRef<OsStr>]) -> Result<()> {
+pub async fn run_script(executable: impl AsRef<OsStr>, args: &[impl AsRef<OsStr>]) -> Result<()> {
     // Run given script to get exit code and return true on success.
     // Return Err on failure, but also print an error if needed
     match script_exit_code(executable, args).await {
@@ -49,8 +46,8 @@ pub async fn run_script<P: AsRef<OsStr>>(executable: P, args: &[impl AsRef<OsStr
 }
 
 #[cfg(not(test))]
-pub async fn script_output<P: AsRef<OsStr>>(
-    executable: P,
+pub async fn script_output(
+    executable: impl AsRef<OsStr>,
     args: &[impl AsRef<OsStr>],
 ) -> Result<String> {
     // Run given command and return the output given
@@ -63,8 +60,8 @@ pub async fn script_output<P: AsRef<OsStr>>(
 }
 
 #[cfg(test)]
-pub async fn script_output<P: AsRef<OsStr>>(
-    executable: P,
+pub async fn script_output(
+    executable: impl AsRef<OsStr>,
     args: &[impl AsRef<OsStr>],
 ) -> Result<String> {
     let test = crate::testing::current();
