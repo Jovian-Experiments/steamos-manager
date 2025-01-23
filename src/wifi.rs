def8d180cb49cbb99c6d80c36b9c87d75089e736
Vicki Pfau <vi@endrift.com>
2025-01-22 17:48:05 -0800

    wifi: Set/clear ath11k debug_mask before/after tracing

--- src/wifi.rs
+++ src/wifi.rs
@@ -124,12 +124,14 @@ async fn restart_iwd(connection: Connection) -> Result<()> {
 }
 
 async fn stop_tracing() -> Result<()> {
-    run_script(TRACE_CMD_PATH, &["stop"]).await
+    run_script(TRACE_CMD_PATH, &["stop"]).await?;
+    Ok(fs::write(path("/sys/module/ath11k/parameters/debug_mask"), b"0\n").await?)
 }
 
 async fn start_tracing(buffer_size: u32) -> Result<()> {
     // Start tracing
     let size_str = buffer_size.to_string();
+    fs::write(path("/sys/module/ath11k/parameters/debug_mask"), b"0xffffefff\n").await?;
     run_script(
         TRACE_CMD_PATH,
         &["start", "-e", "ath11k_wmi_diag", "-b", &size_str],
